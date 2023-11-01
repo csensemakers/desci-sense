9e2c2be31e5c7660d2c78edd9d895112ceb75f39
Ronen Tamari <ronen.tamari1@mail.huji.ac.il>
2023-11-01 11:06:10 -0400

    initial commit for twitter demo

---
+++ twitter.py
@@ -0,0 +1,25 @@
+  
+# Twitter scraping based on https://github.com/JustAnotherArchivist/snscrape/issues/996#issuecomment-1777981568
+
+from typing import Optional
+
+import requests
+
+
+def do_work(data: dict) -> None:
+    # do actual work with data
+    return data
+
+
+def scrape_tweet(tweet_id: str | int) -> Optional[dict]:
+    response = requests.get(url=f"https://api.vxtwitter.com/Twitter/status/{tweet_id}")
+    if not response.ok:
+        print("Couldn't get tweet.")
+        return
+    try:
+        data = do_work(response.json())
+        return data
+    except requests.JSONDecodeError:
+        print("Couldn't decode response.")
+        return
+
