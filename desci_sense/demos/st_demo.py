8f4454ccfb9f7ff3f74524ee8db81e522a57ad78
Ronen Tamari <ronen.tamari1@mail.huji.ac.il>
2023-11-02 17:02:14 -0400

    Update st_demo.py

--- desci_sense/demos/st_demo.py
+++ desci_sense/demos/st_demo.py
@@ -55,7 +55,7 @@ def process_tweet(tweet_url, api_key, openai_referer):
 with st.form("myform"):
     api_key = st.text_input("Enter OpenRouter API Key:", "")
     openapi_referer = st.text_input("Enter OpenRouter Referer:", "https://ai-nanopub.streamlit.app/")
-    tweet_url = st.text_input("Enter Twitter post URL:", "")
+    tweet_url = st.text_input("Enter Twitter post URL:", "https://twitter.com/ClaypoolLab/status/1720165099992961224")
     submitted = st.form_submit_button("Submit")
     if submitted:
         process_tweet(tweet_url, api_key, openapi_referer)
