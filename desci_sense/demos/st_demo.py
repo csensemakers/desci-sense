5d0f40f2d3095f262f8cbdf233df4ca2ab72ce96
Ronen Tamari <ronen.tamari1@mail.huji.ac.il>
2023-11-02 11:43:47 -0400

    add requirements

--- desci_sense/demos/st_demo.py
+++ desci_sense/demos/st_demo.py
@@ -1,10 +1,6 @@
 import sys
 from pathlib import Path
 sys.path.append(str(Path(__file__).parents[2]))
-print(sys.path)
-
-
-
 
 import streamlit as st
 from desci_sense.parsers.base_parser import BaseParser
@@ -12,31 +8,11 @@ from desci_sense.twitter import scrape_tweet
 
 st.title("LLM Nanopublishing assistant demo")
 
-# api_key, selected_model = sidebar(constants.OPENROUTER_DEFAULT_INSTRUCT_MODEL)
-
-# def blog_outline(topic):
-#     # Instantiate LLM model
-#     chat = ChatOpenAI(
-#         model_name=selected_model,
-#         openai_api_key=api_key,
-#         openai_api_base=constants.OPENROUTER_API_BASE,
-#         headers={"HTTP-Referer": constants.OPENROUTER_REFERRER},
-#     )
-#     # Prompt
-#     template="As an experienced data scientist and technical writer, generate an outline for a blog about {topic}."
-#     system_message_prompt = SystemMessagePromptTemplate.from_template(template)
-#     chat_prompt = ChatPromptTemplate.from_messages([system_message_prompt])
-#     prompt = chat_prompt.format_prompt(topic=topic).to_messages()
-#     st.write(chat(prompt))
-
-
-
 
 section_title = "### 🐦 Extracted Tweet"
-
-
-
 result_title = "### 🤖 Nanopub Parser Prediction"
+
+
 
 def print_tweet(tweet):
 
