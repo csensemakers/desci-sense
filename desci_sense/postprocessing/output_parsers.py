df710ca7bba1f5193e856c77cb73e4a270c4c3d3
Ronen Tamari <ronen.tamari1@mail.huji.ac.il>
2023-12-24 22:06:27 -0500

    unify tags lookup

--- desci_sense/postprocessing/output_parsers.py
+++ desci_sense/postprocessing/output_parsers.py
@@ -3,6 +3,8 @@ import re
 from typing import List
 from langchain.schema import BaseOutputParser
 
+from ..prompting.post_tags_pydantic import PostTagsDataModel
+
 def convert_string_to_list(input_string):
     """ 
     convert a string representing a list of names into a list of the names. 
@@ -32,7 +34,7 @@ class TagTypeParser(BaseOutputParser):
 
     @classmethod
     def tags(cls):
-        return ["announce", "reading", "event", "review", "recommendation", "listening", "job", "quote", "discussion"]
+        return PostTagsDataModel.tags()
 
     def parse(self, text: str):
         """Parse the output of an LLM call."""
