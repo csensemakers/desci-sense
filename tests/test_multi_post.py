374f0c24964c5899ca3042dbd6d4f73f1b51b0e1
Ronen Tamari <ronen.tamari1@mail.huji.ac.il>
2023-12-26 16:57:07 -0500

    #51 add test

--- tests/test_multi_post.py
+++ tests/test_multi_post.py
@@ -8,11 +8,19 @@ sys.path.append(str(ROOT))
 
 
 from desci_sense.schema.post import MultiTagRefPost
+from desci_sense.postprocessing.output_parsers import TagTypeParser
 
 
 def test_simple_1():
     post = MultiTagRefPost(author='Jane Doe', content='This is a tagged post.', url='http://example.com', ref_urls=['www.ref1.com'], tags={'event', 'job', 'unknown-tag'})
     assert post.tags == {'event', 'job'}
+
+
+def test_unique_tags():
+    tag_parser = TagTypeParser()
+    text = "Final Answer: \n <event> <discussion> <discussion>"
+    answer = tag_parser.parse(text)
+    assert set(answer["multi_tag"]) == set(["event", "discussion"])
 
 if __name__ == "__main__":
     post = MultiTagRefPost(author='Jane Doe', content='This is a tagged post.', url='http://example.com', tags={'event', 'job', 'unknown-tag'})
