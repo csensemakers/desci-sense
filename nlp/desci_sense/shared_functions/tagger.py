034405c89e5338f46b0e79e0494097c26424399c
Ronen Tamari <ronen.tamari1@mail.huji.ac.il>
2024-02-06 19:03:36 -0500

    #103 fix hard import
    TODO check if there is a less hacky way

--- nlp/desci_sense/shared_functions/tagger.py
+++ nlp/desci_sense/shared_functions/tagger.py
@@ -1,18 +1,20 @@
 from pathlib import Path
-# APP_ROOT = Path(__file__).parents[3]
-# NLP_PATH = APP_ROOT / "nlp"
+APP_ROOT = Path(__file__).parents[4]
+NLP_PATH = APP_ROOT / "nlp"
 import sys
 
-# TODO fix this hard import!
-sys.path.insert(1, "/Users/ronentamari/Documents/dev/common_sense/sensemaker_app/firebase_branch/nlp")
+# TODO check if there is a less hacky way
+sys.path.insert(1, str(NLP_PATH))
 
 from loguru import logger
+
 
 from desci_sense.parsers.firebase_api_parser import FirebaseAPIParser
 from desci_sense.configs import init_multi_stage_parser_config, environ
 # from ..configs import init_multi_stage_parser_config
 
 def SM_FUNCTION_post_tagger_imp(content, parameters) -> dict:
+    logger.info(str(NLP_PATH))
     config = init_multi_stage_parser_config()
     parser = FirebaseAPIParser(config, 
                                openai_api_key=environ.get("OPENROUTER_API_KEY"),
