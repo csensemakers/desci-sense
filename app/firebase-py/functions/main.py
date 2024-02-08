baffcc7ca46dc39664176f64602b7b5ad1361e90
pepo <pepo.ospina@gmail.com>
2024-02-08 11:13:11 +0100

    black formater workin

--- app/firebase-py/functions/main.py
+++ app/firebase-py/functions/main.py
@@ -9,13 +9,15 @@ from config import openai_api_key
 app = initialize_app()
 
 
-
 @https_fn.on_request(min_instances=1)
-def SM_FUNCTION_post_parser(request):
+def sm_function_post_parser(request):
+    """
+    Wrapper on SM_FUNCTION_post_parser_imp
+    """
     request_json = request.get_json()
-    content =  request_json['content'];
-    parameters =  request_json['parameters'];
-    
+    content = request_json["content"]
+    parameters = request_json["parameters"]
+
     config: SM_FUNCTION_post_parser_config = {
         "wandb_project":   "st-demo-sandbox",
         "max_summary_length": 500,
@@ -23,12 +25,11 @@ def SM_FUNCTION_post_parser(request):
         "openai_api_base": "https://openrouter.ai/api/v1",
         "openai_api_referer": "https://127.0.0.1:3000/"
     }
-    
+
     semantics = SM_FUNCTION_post_parser_imp(content, parameters, config)
-    
+
     return https_fn.Response(
         json.dumps({"semantics": semantics }),
         status=200,
         headers={"Content-Type": "application/json"}
     )
-    
