6a7035d448a4211e0f8737860657408f0ca671c4
pepo <pepo.ospina@gmail.com>
2024-02-09 12:27:50 +0100

    using patterns

--- app/webapp/src/semantics/SemanticsEditor.tsx
+++ app/webapp/src/semantics/SemanticsEditor.tsx
@@ -5,11 +5,14 @@ import { ParserResult } from '../shared/types';
 import { LoadingDiv } from '../ui-components/LoadingDiv';
 import { renderPatterns } from './patterns/patterns';
 
+const DEBUG = false;
+
 export const SemanticsEditor = (props: {
   isLoading: boolean;
   parsed?: ParserResult;
 }) => {
   const patterns = useMemo<JSX.Element[]>(() => {
+    if (DEBUG) console.log('patterns', { parsed: props.parsed });
     if (!props.parsed || !props.parsed.semantics) {
       return [];
     }
